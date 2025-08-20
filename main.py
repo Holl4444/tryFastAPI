9a098bd76141d15748942eb256c834b54252816e
Holl4444 <holly.cello@gmail.com>
2025-08-20 14:06:03 +0100

    add: post request

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 # new path syntax
 @app.get('/')
 def root():
     return {'Hello': 'World'}
+
+@app.post('/items')
+# item = query parameter
+def create_item(item: str):
+    items.append(item)
+    return item
